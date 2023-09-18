a3aa31b7efd2989ac64a9f05819cedcbdaa79722
ajitdhanukmandal <adjitdhanukmandal@gmail.com>
2023-09-18 21:13:00 +0545

    this is first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//data types
+
+// let length = 5;
+// let myName = 'Ajit';
+// let x = BigInt(123456789789456123456987);
+// let y = true;
+// let c;
+// let d = "";
+// let cars = ["Suzuki", 'Maruti', 'BMW']
+// let student = {firstName: 'Ait', lastName: 'Mandal'};
+
+// console.log(typeof(length));
+// console.log(typeof(myName));
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(cars));
+// console.log(typeof(student));
+
+// let myPhone = 9804064652;
+// let myName = 'Ajit Mandal';
+
+// console.log(isNaN(myPhone));
+// console.log(isNaN(myName));
+
+// console.log(Number.);
+
+// let myPhone = 'Ajit';
+// let myPhone = 98;
+
+// if(isNaN(myPhone)){
+//     console.log("Print give a valid number");
+// }
+
+// let num = 20;
+// let newNum = num++;
+// console.log(num);;
+// console.log(newNum);
+
+// let num = 20;
+// let newNum = ++num;
+// console.log(num);;
+// console.log(newNum);
+
+// console.log(10**-1);
+
+//swapping two numbers
+var a = 5;
+var b = 6;
+
+var c = a + b;
+var a = b;
+var b = c - a;
+
+console.log("The value of a is: " + a  + " and the value of b is: " + b);
